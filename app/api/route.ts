358e72af1d89c9cbfa68f4b169d38ba21dda8d02
ssebastianvv <ssebastianvapi@gmail.com>
2025-01-11 10:54:05 -0500

    change route and finish page

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,23 +1,34 @@
-// arrays 
+import { NextResponse } from 'next/server';
 
+const subjects = ["The dog", "The turtle", "My friend", "Sebastian"];
+const predicates = ["runs fast", "is very wise", "loves coding", "sings poorly"];
 
-const subjects    = ["The dog", "The turtle", "My friend", "Sebastian"];
-
-const predicates  = ["runs fast", "is very wise", "loves coding", "sings poorly"];
-
-function create (subjects,predicates){
-    if (subjects.length>8){
-        subjects+="inteligente"
-    }
-
-    if (predicates.includes("coding")){
-        predicates+= "!"
-    }
-    
-    return subjects+predicates
-}
-for (let i = 0; i < sujetos.length; i++) {
-    const sujeto = sujetos[i];
-    const predicado = predicados[i];
-    console.log(crearOracion(sujeto, predicado));
+function create(subject: string, predicate: string): string {
+  if (subject.length > 8) {
+    subject += " inteligente";
   }
+
+  if (predicate.includes("coding")) {
+    predicate += "!";
+  }
+
+  return subject + " " + predicate;
+}
+
+function getRandomSubject() {
+  const randomIndex = Math.floor(Math.random() * subjects.length);
+  return subjects[randomIndex];
+}
+
+function getRandomPredicate() {
+  const randomIndex = Math.floor(Math.random() * predicates.length);
+  return predicates[randomIndex];
+}
+
+export async function GET() {
+  const subject = getRandomSubject();
+  const predicate = getRandomPredicate();
+  const phrase = create(subject, predicate);
+
+  return NextResponse.json({ phrase });
+}
